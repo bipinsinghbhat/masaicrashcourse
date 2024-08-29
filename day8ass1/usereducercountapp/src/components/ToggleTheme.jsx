9eb272f72992b89d0c61148004c2bfe7d96d4cbc
bipinsinghbhat <bipinsinghbhat1698@gmail.com>
2024-08-29 20:02:56 +0530

    p

--- day8ass1/usereducercountapp/src/components/ToggleTheme.jsx
+++ day8ass1/usereducercountapp/src/components/ToggleTheme.jsx
@@ -6,11 +6,11 @@ const initialstate={
    
 }
 
-const TOGGLE_THEME="TOGGLE_THEME"
+
 
 const reducer=(state,action)=>{
          switch(action.type){
-           case TOGGLE_THEME:
+           case "TOGGLE_THEME":
             return {
                ...state, theme:state.theme==="light"? "dark":"light"
             }
